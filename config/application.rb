7e318d80608b930affc6992da1e28cc70fe2193d
Charles Marcoin <charles.marcoin@gmail.com>
2024-06-07 14:29:51 +0200

    add dsfr and make it compatible with rails assets pipeline

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 
+# require_relative '../lib/middleware/static_assets'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -23,5 +25,8 @@ module SignauxFaiblesV2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Add custom middleware to serve assets from desired paths
+    # config.middleware.use Middleware::StaticAssets
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,7 @@
 # Pin npm packages by running ./bin/importmap
-
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "dsfr", to: "vendor/javascript/dsfr.module.min.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "dsfr", to: "dsfr.module.min.js"
