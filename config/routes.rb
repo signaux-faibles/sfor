c42a60a09212f561a51a893dbd2370c43e8b33c5
Charles Marcoin <charles.marcoin@gmail.com>
2024-06-07 14:27:56 +0200

    added home page and basic establishments views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,5 +9,5 @@ Rails.application.routes.draw do
   resources :establishments, only: [:index, :show], path: :entreprises
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "pages#home"
 end
