fd3d7b5083a2267cf1b5b820209a735f169b4b6e
Charles Marcoin <charles.marcoin@gmail.com>
2024-10-10 14:56:32 +0200

    Add breadcrumb navigation to establishment views
    Introduced a breadcrumb helper in `application_helper.rb` to display navigation paths. Integrated this helper into `show.html.erb` for both establishment trackings and establishments to improve user navigation and experience.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,44 @@ module ApplicationHelper
       flash_type.to_s
     end
   end
+
+  def breadcrumb
+    content_tag(:nav, role: 'navigation', class: 'fr-breadcrumb', aria: { label: 'vous êtes ici :' }) do
+      safe_join([
+                  button_tag('Voir le fil d’Ariane', class: 'fr-breadcrumb__button', aria: { expanded: 'false', controls: 'breadcrumb-1' }),
+                  content_tag(:div, class: 'fr-collapse', id: 'breadcrumb-1') do
+                    content_tag(:ol, class: 'fr-breadcrumb__list') do
+                      safe_join(
+                        breadcrumb_items.map do |text, path, current|
+                          content_tag(:li) do
+                            if current
+                              content_tag(:a, text, class: 'fr-breadcrumb__link', 'aria-current': 'page')
+                            else
+                              link_to(text, path, class: 'fr-breadcrumb__link')
+                            end
+                          end
+                        end
+                      )
+                    end
+                  end
+                ])
+    end
+  end
+
+  private
+
+  def breadcrumb_items
+    items = []
+    items << ['Accompagnements', establishment_trackings_path]
+
+    if controller_name == 'establishments' && action_name == 'show'
+      items << ['Historique des accompagnements', nil, true]
+    elsif controller_name == 'establishment_trackings' && action_name == 'show'
+      establishment = @establishment_tracking&.establishment
+      items << ['Historique des accompagnements', establishment_path(establishment)] if establishment
+      items << ["Accompagnement - #{establishment&.raison_sociale}", establishment_establishment_tracking_path(establishment, @establishment_tracking), true]
+    end
+
+    items
+  end
 end
