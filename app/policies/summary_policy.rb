d99e60e57308f2cc236eabdbfe9a80d815cfbf1f
Charles Marcoin <charles.marcoin@gmail.com>
2024-10-17 14:09:02 +0200

    added a policy for summaries

--- app/policies/summary_policy.rb
+++ app/policies/summary_policy.rb
@@ -7,14 +7,34 @@ class SummaryPolicy < ApplicationPolicy
   end
 
   def show?
+    record_matches_user_segment_and_department?
+  end
+
+  def create?
+    record_matches_user_segment_and_department?
+  end
+
+  def update?
+    record_matches_user_segment_and_department?
+  end
+
+  def edit?
+    record_matches_user_segment_and_department?
+  end
+
+  def cancel?
+    record_matches_user_segment_and_department?
+  end
+
+  private
+
+  def record_matches_user_segment_and_department?
     if record.segment.present?
       record.segment == user.segment && department_match?
     else
       department_match?
     end
   end
-
-  private
 
   def department_match?
     record.establishment_tracking.establishment.department_id.in?(user.department_ids)
