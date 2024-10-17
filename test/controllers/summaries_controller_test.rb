b5833a5921d8af8e9d4c776a0b7b9f7269cd2f1b
Charles Marcoin <charles.marcoin@gmail.com>
2024-10-17 11:37:00 +0200

    Add summary locking integration tests
    This commit introduces tests for locking and editing summaries by different users. It ensures that one user can lock a summary and another user is prevented from editing it. Additionally, it verifies that the summary can be unlocked and subsequently edited by another user.

---
+++ test/controllers/summaries_controller_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class SummariesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @establishment_tracking = establishment_trackings(:establishment_tracking_paris)
+    @establishment = @establishment_tracking.establishment
+    @summary = summaries(:summary_paris_crp)
+
+    @user_a = users(:user_crp_paris)
+    @user_b = users(:user_crp_paris_2)
+
+  end
+
+  test "user A can lock and edit the summary" do
+    sign_in @user_a
+
+    get edit_establishment_establishment_tracking_summary_path(@establishment, @establishment_tracking, @summary), as: :turbo_stream
+
+    @summary.reload
+    assert_equal @user_a.id, @summary.locked_by
+    assert @summary.locked?
+
+    assert_includes @response.body, @summary.content
+  end
+
+  test "user B cannot edit a summary locked by user A" do
+    sign_in @user_a
+
+    get edit_establishment_establishment_tracking_summary_path(@establishment, @establishment_tracking, @summary)
+
+    sign_out @user_a
+    sign_in @user_b
+
+    get establishment_establishment_tracking_path(@establishment, @establishment_tracking)
+
+    assert_select "button[disabled]", text: "En cours d'édition par #{@user_a.email}"
+  end
+
+  test "user A unlocks the summary and user B can edit it" do
+      sign_in @user_a
+      get edit_establishment_establishment_tracking_summary_path(@establishment, @establishment_tracking, @summary), as: :turbo_stream
+
+      @summary.reload
+      assert_equal @user_a.id, @summary.locked_by
+      assert @summary.locked?
+
+      get cancel_establishment_establishment_tracking_summary_path(@establishment, @establishment_tracking, @summary), as: :turbo_stream
+
+      @summary.reload
+      assert_nil @summary.locked_by
+      assert_not @summary.locked?
+
+      sign_in @user_b
+      get edit_establishment_establishment_tracking_summary_path(@establishment, @establishment_tracking, @summary), as: :turbo_stream
+
+      assert_includes @response.body, @summary.content
+  end
+end
