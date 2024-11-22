13a04cae2fc3ec70075ae99d03430f50778932c7
Charles Marcoin <charles.marcoin@gmail.com>
2024-11-22 16:56:24 +0100

    redesigned filters

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,10 +1,21 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = ["hiddenFilters"];
+    static targets = ["hiddenFilters", "toggleButton", "filtersOpenInput"];
 
     toggle() {
+        console.log("Toggling")
         const isHidden = this.hiddenFiltersTarget.style.display === "none";
+
+        // Toggle hidden filters visibility
         this.hiddenFiltersTarget.style.display = isHidden ? "block" : "none";
+
+        // Update the button's icon and label dynamically
+        this.toggleButtonTarget.innerHTML = isHidden
+            ? `<span class="fr-icon-arrow-up-line" aria-hidden="true"></span> Réduire les critères supplémentaires`
+            : `<span class="fr-icon-arrow-down-line" aria-hidden="true"></span> Afficher des critères supplémentaires`;
+
+        // Update the hidden input field value
+        this.filtersOpenInputTarget.value = isHidden ? "true" : "false";
     }
 }
