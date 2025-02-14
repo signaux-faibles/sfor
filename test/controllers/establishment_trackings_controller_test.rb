4d681ad0d6c3f381246cf1f38eca2bd76c553a2f
Charles Marcoin <charles.marcoin@gmail.com>
2025-02-14 11:54:09 +0100

    Add network-based filtering for establishment trackings
    Introduced new filters to allow users to see establishment trackings within their networks. Added corresponding scopes in the model, updated controller logic, and enhanced the UI with a new filter option. Included tests and fixtures to ensure functionality and reliability.

--- test/controllers/establishment_trackings_controller_test.rb
+++ test/controllers/establishment_trackings_controller_test.rb
@@ -1,9 +1,11 @@
+require 'test_helper'
 class EstablishmentTrackingsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   setup do
     @establishment_tracking_paris = establishment_trackings(:establishment_tracking_paris)
     @establishment_tracking_finistere = establishment_trackings(:establishment_tracking_finistere)
+    @establishment_tracking_paris_with_urssaf_referents = establishment_trackings(:establishment_tracking_paris_with_urssaf_referents)
 
     sign_in users(:user_crp_paris)
   end
@@ -13,5 +15,20 @@ class EstablishmentTrackingsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_includes response.body, @establishment_tracking_paris.establishment.raison_sociale
     assert_not_includes response.body, @establishment_tracking_finistere.establishment.raison_sociale
+  end
+
+  test "should only include establishment_trackings within user's networks when my network filter is used" do
+    params = {
+      q: {
+        my_tracking: "network",
+      },
+    }
+    get establishment_trackings_url, params: params
+
+    assert_response :success
+
+    assert_includes response.body, @establishment_tracking_paris.establishment.raison_sociale
+    assert_not_includes response.body, @establishment_tracking_finistere.establishment.raison_sociale
+    assert_not_includes response.body, @establishment_tracking_paris_with_urssaf_referents.establishment.raison_sociale
   end
 end
