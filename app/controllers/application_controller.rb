488c23aa514299e4907b2d0487a8fd65af4be923
Charles Marcoin <charles.marcoin@gmail.com>
2025-02-19 10:28:07 +0100

    Add user segment restriction in ApplicationController
    Introduce a `check_user_segment` before_action to restrict access for users in the 'sf' segment. Ineligible users are redirected to the unauthorized page with an appropriate alert message. This ensures proper authorization based on user segmentation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   impersonates :user
   around_action :set_time_zone
   before_action :authenticate_user!
+  before_action :check_user_segment
   before_action :set_sentry_user
 
   include Pundit::Authorization
@@ -18,6 +19,14 @@ class ApplicationController < ActionController::Base
         flash[:alert] = "Vous n'êtes pas autorisé à effectuer cette action."
         redirect_back(fallback_location: root_path)
       end
+    end
+  end
+
+  def check_user_segment
+    return if devise_controller? || request.path == unauthorized_path
+
+    if current_user && current_user.segment.name == 'sf'
+      redirect_to unauthorized_path, alert: "Vous n'êtes pas autorisé à accéder à cette section."
     end
   end
 
