741b66053fc79a5a8361c87c4da666305d02118f
Charles Marcoin <charles.marcoin@gmail.com>
2025-03-13 11:21:37 +0100

    Add minimum input threshold for user selection fields
    Updated the user selection fields to require at least 3 characters before searching, improving usability and performance. Adjusted the frontend TomSelect controller to incorporate the threshold and updated the corresponding views for better UX. Removed unused database tables and foreign key constraints for cleanup.

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -3,22 +3,31 @@ import { Controller } from "@hotwired/stimulus"
 // Tom Select est déjà  chargé globalement par Importmap
 
 export default class extends Controller {
+
+
+
+
+
+
     connect() {
         // Fix the issue of the tom-select instance not being initialized when hitting back button of the browser
         if (this.element.tomselect) {
             this.element.tomselect.destroy();
         }
 
-        this.tomSelectInstance = new TomSelect(this.element, {
+
+        const minSearchAttr = this.element.dataset.minSearch;
+        const threshold = minSearchAttr ? parseInt(minSearchAttr, 10) : 1;
+
+        const options = {
+            clearOnBlur: false,
+            persist: true,
+            closeAfterSelect: true,
             plugins: ['remove_button'],
             maxOptions: null,
-            persist: false,
             create: false,
             dropdownParent: 'body',
-            sortField: {
-                field: "text",
-                direction: "asc"
-            },
+            openOnFocus: !minSearchAttr,
             render: {
                 option: function(data, escape) {
                     return `<div class="option" style="margin-left: 10px;">${escape(data.text)}</div>`;
@@ -31,19 +40,33 @@ export default class extends Controller {
                 },
                 no_results: function(data, escape) {
                     return '<div class="no-results">' + 'Aucun résultat' + '</div>';
-                }
+                },
             },
+
             onItemAdd: function(value, item) {
                 this.setTextboxValue('');
                 this.refreshOptions();
+            },
+
+            onType: function(str) {
+                if (str.length >= threshold) {
+                    this.refreshOptions(true);
+                } else {
+                    this.close();
+                    this.setTextboxValue(str);
+                }
             }
-        });
+        }
+
+        this.tomSelectInstance = new TomSelect(this.element, options);
+
     }
-    
+
     // Fix the issue of the tom-select instance not being initialized when hitting back button of the browser
     disconnect() {
         if (this.tomSelectInstance) {
             this.tomSelectInstance.destroy();
         }
     }
+
 }
