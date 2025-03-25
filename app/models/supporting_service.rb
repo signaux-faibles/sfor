86fd39339261f471eb5a53e442161f5df94fa81b
Charles Marcoin <charles.marcoin@gmail.com>
2025-03-25 17:30:13 +0100

    Enhance establishment tracking and supporting services functionality
    Updated the EstablishmentTracking model to include supporting services in the ransackable associations. Modified the SupportingService model to enforce uniqueness on the name attribute and added ransackable attributes for improved querying. Additionally, updated the filters view to allow selection of supporting services, enhancing the user interface for tracking management.

--- app/models/supporting_service.rb
+++ app/models/supporting_service.rb
@@ -2,5 +2,9 @@ class SupportingService < ApplicationRecord
   has_and_belongs_to_many :establishment_trackings, 
                          join_table: :establishment_tracking_supporting_services
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "id_value", "name", "updated_at"]
+  end
 end 
