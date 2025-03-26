8cb11befb1aac0c508dd4198897ea6f80455f7a5
Charles Marcoin <charles.marcoin@gmail.com>
2025-03-26 10:44:44 +0100

    Add position attribute to CodefiRedirects and implement ordering scope
    Introduced a new migration to add a 'position' integer column to the codefi_redirects table with a default value of 0. Updated the CodefiRedirect model to include a default scope and a named scope for ordering by the position attribute in ascending order. Modified views to utilize the ordered scope for consistent display of codefi redirects in forms and info panels, enhancing organization and clarity.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_26_085418) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_26_093418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_26_085418) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position", default: 0, null: false
   end
 
   create_table "codefi_redirects_establishment_trackings", id: false, force: :cascade do |t|
