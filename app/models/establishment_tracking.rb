c615515d58e99a7330dada6ddd50aa09985459e8
Charles Marcoin <charles.marcoin@gmail.com>
2025-04-07 17:44:44 +0200

    Add discared contributors management features to establishment tracking
    Implemented functionality to remove referents and participants from establishment tracking. Introduced new methods in the EstablishmentTrackingsController and added a ContributorsManageable concern to encapsulate contributor management logic. Updated views to display archived contributors with options to permanently delete them. Enhanced the establishment tracking model to include associations for discarded referents and participants, and updated routes and policies accordingly. This improves user experience and management capabilities for contributors.

--- app/models/establishment_tracking.rb
+++ app/models/establishment_tracking.rb
@@ -9,9 +9,11 @@ class EstablishmentTracking < ApplicationRecord
 
   has_many :tracking_referents, dependent: :destroy
   has_many :referents, through: :tracking_referents, source: :user
+  has_many :discarded_referents, -> { discarded }, through: :tracking_referents, source: :user
 
   has_many :tracking_participants, dependent: :destroy
   has_many :participants, through: :tracking_participants, source: :user
+  has_many :discarded_participants, -> { discarded }, through: :tracking_participants, source: :user
 
   has_many :establishment_tracking_labels, dependent: :destroy
   has_many :tracking_labels, through: :establishment_tracking_labels
