a2706f64966db87dae852d6470dcf3076ba241ae
Charles Marcoin <charles.marcoin@gmail.com>
2025-07-04 11:33:27 +0200

    don't run prometheus exporter in test env

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,9 +1,11 @@
-require "prometheus_exporter/middleware"
+unless Rails.env.test?
+  require "prometheus_exporter/middleware"
 
-# Config for PrometheusExporter to connect to the exporter service
-PrometheusExporter::Client.default = PrometheusExporter::Client.new(
-  host: ENV.fetch("PROMETHEUS_EXPORTER_HOST", "localhost"),
-  port: ENV.fetch("PROMETHEUS_EXPORTER_PORT", 9394)
-)
+  # Config for PrometheusExporter to connect to the exporter service
+  PrometheusExporter::Client.default = PrometheusExporter::Client.new(
+    host: ENV.fetch("PROMETHEUS_EXPORTER_HOST", "localhost"),
+    port: ENV.fetch("PROMETHEUS_EXPORTER_PORT", 9394)
+  )
 
-Rails.application.middleware.unshift PrometheusExporter::Middleware
+  Rails.application.middleware.unshift PrometheusExporter::Middleware
+end
