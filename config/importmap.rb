66daea4c9d583ad56afb4d7aef423a6ad0b67eb2
Charles Marcoin <charles.marcoin@gmail.com>
2025-09-12 14:43:28 +0200

    Update asset management by adding application.js pin to importmap.rb and clean up comments in assets.rb for clarity.

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,11 +1,12 @@
 # Pin npm packages by running ./bin/importmap
+pin "application", to: "application.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
 pin "dsfr", to: "dsfr.module.min.js"
 pin "tom-select", to: "tom-select.js"
 pin "ahoy" # @1.0.1
 pin "track_tabs", to: "track_tabs.js"
 pin "os" # @2.1.0
 pin "dsfr-chart", to: "dsfr-chart.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
