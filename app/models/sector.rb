c3a5620e97c7abde904811328224c427f2d59907
Charles Marcoin <charles.marcoin@gmail.com>
2025-09-29 16:26:29 +0200

    Add ordered_by_name scope to Sector model and update establishment tracking forms to use it for consistent sector sorting

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -1,6 +1,8 @@
 class Sector < ApplicationRecord
   has_and_belongs_to_many :establishment_trackings
 
+  scope :ordered_by_name, -> { all.sort_by { |sector| I18n.transliterate(sector.name.downcase) } }
+
   def self.ransackable_attributes(_auth_object = nil)
     %w[id name]
   end
