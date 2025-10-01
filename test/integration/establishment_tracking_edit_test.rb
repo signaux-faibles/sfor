e01c34b34bf93556d6fdbab21bf8077053b8fcca
Charles Marcoin <charles.marcoin@gmail.com>
2025-10-01 15:51:37 +0200

    added integration test for user action dynamic attribution

--- test/integration/establishment_tracking_edit_test.rb
+++ test/integration/establishment_tracking_edit_test.rb
@@ -11,4 +11,25 @@ class EstablishmentTrackingEditTest < ActionDispatch::IntegrationTest
 
     @redirect_action_name = "Réorientation externe au codéfi"
   end
+
+  test "editing an establishment_tracking with a CodefiRedirect automatically adds the correct UserAction" do
+    assert_not @establishment_tracking.user_actions.exists?(name: @redirect_action_name)
+
+    patch establishment_establishment_tracking_path(@establishment_tracking.establishment, @establishment_tracking), params: {
+      establishment_tracking: {
+        codefi_redirect_ids: [@codefi_redirect.id],
+        tracking_label_ids: [],
+        state: @establishment_tracking.state
+      }
+    }
+
+    assert_response :redirect
+    follow_redirect!
+
+    @establishment_tracking.reload
+
+    assert_includes @establishment_tracking.codefi_redirect_ids, @codefi_redirect.id
+
+    assert @establishment_tracking.user_actions.exists?(name: @redirect_action_name), "Expected the UserAction '#{@redirect_action_name}' to be added"
+  end
 end
