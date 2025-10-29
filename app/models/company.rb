d1e70b2fbb093ed7b3ec35ed25c54fd1f62274c2
Charles Marcoin <charles.marcoin@gmail.com>
2025-10-29 10:47:12 +0100

    refactor(companies): reshape schema by removing unused fields and updating data types

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,4 @@
 class Company < ApplicationRecord
-  belongs_to :department
   belongs_to :activity_sector, optional: true
   has_many :establishments, foreign_key: :siren, primary_key: :siren, dependent: :nullify
 
@@ -10,14 +9,13 @@ class Company < ApplicationRecord
   has_many :lists, through: :company_lists
 
   validates :siren, presence: true, uniqueness: true
-  validates :siret, presence: true, uniqueness: true
 
   def self.ransackable_attributes(_auth_object = nil)
-    %w[created_at id id_value siren siret raison_sociale effectif updated_at]
+    %w[created_at id id_value siren raison_sociale statut_juridique creation updated_at]
   end
 
   def self.ransackable_associations(_auth_object = nil)
-    %w[campaign_companies campaigns company_lists establishments lists department activity_sector]
+    %w[campaign_companies campaigns company_lists establishments lists]
   end
 
   def establishments_ordered
