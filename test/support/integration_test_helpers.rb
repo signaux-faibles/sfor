c1cd23427c7d078e390206e3ecf576ee63cfb775
Charles Marcoin <charles.marcoin@gmail.com>
2025-11-21 10:14:24 +0100

    fix tests login helper

--- test/support/integration_test_helpers.rb
+++ test/support/integration_test_helpers.rb
@@ -7,7 +7,9 @@ module IntegrationTestHelpers
         password: "password"
       }
     }
-    assert_response :success, "Failed to sign in user #{user.email}"
+    # Devise redirects after successful login (302), so we check for redirect or success
+    assert_response :redirect,
+                    "Failed to sign in user #{user.email}. Response: #{@response.status} - #{@response.body[0..200]}"
 
     follow_redirect! if @response.redirect?
   end
