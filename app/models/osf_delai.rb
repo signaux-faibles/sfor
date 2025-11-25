2ea0c2e31355e468c9007aa0019d8e70e52f3e1f
Charles Marcoin <charles.marcoin@gmail.com>
2025-11-25 10:56:06 +0100

    add active URSSAF debt status to company establishments

--- app/models/osf_delai.rb
+++ app/models/osf_delai.rb
@@ -2,4 +2,9 @@ class OsfDelai < ApplicationRecord
   belongs_to :establishment, foreign_key: :siret, primary_key: :siret, optional: true
 
   validates :siret, presence: true, length: { is: 14 }
+
+  def self.active_dette_urssaf?(siret)
+    where(siret: siret)
+      .exists?(["date_echeance > ?", Date.current])
+  end
 end
