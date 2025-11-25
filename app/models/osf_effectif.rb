edc0020d0925958a29c42fbd75a3f283b1cc14e8
Charles Marcoin <charles.marcoin@gmail.com>
2025-11-25 10:27:41 +0100

    calculat last effectif in company establishments list

--- app/models/osf_effectif.rb
+++ app/models/osf_effectif.rb
@@ -3,4 +3,11 @@ class OsfEffectif < ApplicationRecord
 
   validates :siret, presence: true, length: { is: 14 }
   validates :periode, presence: true
+
+  def self.last_effectif_for_siret(siret)
+    where(siret: siret)
+      .order(periode: :desc)
+      .limit(1)
+      .pick(:effectif)
+  end
 end
