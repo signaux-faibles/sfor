e918e0b0956991866ed9ec33b0607a6c6b2c61c8
Charles Marcoin <charles.marcoin@gmail.com>
2025-11-27 11:04:39 +0100

    linting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,9 +16,9 @@ class ApplicationController < ActionController::Base
 
   def layout_by_resource
     if devise_controller?
-      'devise'
+      "devise"
     else
-      'application'
+      "application"
     end
   end
 
