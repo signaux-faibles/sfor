69808aa90dcb20b5a565daced146ead2e1a85ef3
Charles Marcoin <charles.marcoin@gmail.com>
2025-12-08 11:19:03 +0100

    admins can generate reset password urls if needed

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < Admin::ApplicationController
-  before_action :set_user, only: %i[show impersonate]
+  before_action :set_user, only: %i[show impersonate reset_password]
 
   def index
     @users = User.includes(:entity, :segment)
@@ -20,6 +20,15 @@ class Admin::UsersController < Admin::ApplicationController
       impersonate_user(@user)
       redirect_to root_path, notice: "Vous êtes maintenant connecté en tant que #{@user.email}"
     end
+  end
+
+  def reset_password
+    # Generate a reset password token without sending email
+    raw_token = @user.send(:set_reset_password_token)
+    reset_url = edit_user_password_url(reset_password_token: raw_token)
+    flash[:reset_password_url] = reset_url
+    redirect_to admin_user_path(@user),
+                notice: "Lien de réinitialisation généré. Vous pouvez le copier depuis la page de l'utilisateur." # rubocop:disable Rails/I18nLocaleTexts
   end
 
   private
