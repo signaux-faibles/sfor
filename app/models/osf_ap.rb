2e6f71d0beeac7bbbcd8720cf5f7ad637194bed3
Charles Marcoin <charles.marcoin@gmail.com>
2025-12-11 15:02:36 +0100

    ap sync service does not check for establishments existence

--- app/models/osf_ap.rb
+++ app/models/osf_ap.rb
@@ -1,5 +1,5 @@
 class OsfAp < ApplicationRecord
-  belongs_to :establishment, foreign_key: :siret, primary_key: :siret
+  belongs_to :establishment, foreign_key: :siret, primary_key: :siret, optional: true
 
   validates :siret, presence: true, length: { is: 14 }
   validates :siren, presence: true, length: { is: 9 }
