5d677d281610d5f8897757c5c3d8d40582b86419
Charles Marcoin <charles.marcoin@gmail.com>
2025-12-11 15:57:13 +0100

    migration for sjcf model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_11_135426) do
+ActiveRecord::Schema[7.2].define(version: 2025_12_11_143512) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -535,6 +535,16 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_11_135426) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sjcf_companies", force: :cascade do |t|
+    t.string "siren", limit: 9, null: false
+    t.string "libelle_liste", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["libelle_liste"], name: "index_sjcf_companies_on_libelle_liste"
+    t.index ["siren", "libelle_liste"], name: "index_sjcf_companies_on_siren_and_libelle_liste", unique: true
+    t.index ["siren"], name: "index_sjcf_companies_on_siren"
   end
 
   create_table "summaries", force: :cascade do |t|
