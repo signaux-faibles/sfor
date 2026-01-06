1a970f94398e0c3ac123b3d64b9fb8aac9e1ad33
Charles Marcoin <charles.marcoin@gmail.com>
2026-01-06 16:14:33 +0100

    speed up list loading for users with france entiere access

--- app/policies/company_policy.rb
+++ app/policies/company_policy.rb
@@ -1,6 +1,10 @@
 class CompanyPolicy < ApplicationPolicy
   class Scope < ApplicationPolicy::Scope
     def resolve
+      # If user has access to all departments (France entière), skip filtering
+      return scope if user.geo_access.name.downcase == "france entière"
+
+      # Otherwise, filter by user's departments
       scope.where(department: user.departments.pluck(:code))
     end
   end
