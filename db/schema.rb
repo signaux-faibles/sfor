c8b4a7b84ea4da05714857afa62307a27df4e42b
Charles Marcoin <charles.marcoin@gmail.com>
2026-01-29 16:01:53 +0100

    sjcf filter activation/deactivation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_28_000000) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_29_000000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -404,6 +404,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_28_000000) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "list_date"
+    t.boolean "sjcf_filter_active", default: false, null: false
   end
 
   create_table "network_memberships", force: :cascade do |t|
