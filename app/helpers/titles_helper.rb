5f233fbeafccf284ed5e0b9478ffb052965d3a05
Chloé Corfmat <contact.chloecorfmat@gmail.com>
2026-04-30 10:35:37 +0200

    fix: remove titles logic into helper

---
+++ app/helpers/titles_helper.rb
@@ -0,0 +1,31 @@
+module TitlesHelper
+  def search_page_title(search_params, pagination)
+    query = search_params.dig(:q)
+    page = search_params.dig(:page).to_i
+    total_results = pagination.dig(:total_results).to_i
+    current_page = pagination.dig(:page).to_i
+
+    has_query = query.present?
+    has_page = page > 1
+
+    # No query or page, return default title
+    return "Recherche" unless has_query || has_page
+
+    # Start title.
+    title = has_query ? "Recherche \"#{h(query)}\"".html_safe : "Recherche"
+
+    # Add results count (if needed).
+    results_label = case total_results
+                    when 1 then "1 résultat trouvé"
+                    when (2..) then "#{total_results} résultats trouvés"
+                    else "aucun résultat trouvé"
+                    end
+
+    title += ", #{results_label}"
+
+    # Add page number (if needed).
+    title += ", page #{current_page}" if has_page
+
+    title
+  end
+end
