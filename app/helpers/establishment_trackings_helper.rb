18505bcc9ff9363980919049d7e9c1d86b46f387
Chloé Corfmat <contact.chloecorfmat@gmail.com>
2026-05-07 15:50:37 +0200

    fix: remove unused controller & update helper

--- app/helpers/establishment_trackings_helper.rb
+++ app/helpers/establishment_trackings_helper.rb
@@ -1,15 +1,44 @@
 module EstablishmentTrackingsHelper
+
+  # Public : Build url params for sorting.
   def next_sort_url(params)
-    # Current direction.
-    current_sort = params.dig(:q, :s)
-    # Find next sort direction : asc or desc.
-    next_direction = current_sort == "modified_at desc" ? "asc" : "desc"
-    # Build next url : except view and toto (switch between table and cards view).
-    sort_q = (params[:q]&.permit!&.to_h || {}).merge("s" => "modified_at #{next_direction}").except("view", "toto")
+    sort_q = current_q_params(params)
+    sort_q["s"] = "modified_at #{next_sort_direction(params)}"
     establishment_trackings_path(q: sort_q)
   end
 
+  # Public : Define sort icon class.
   def sort_icon_class(params)
-    params.dig(:q, :s) == "modified_at asc" ? "fr-icon-arrow-up-s-fill" : "fr-icon-arrow-down-s-fill"
+    current_modified_at_direction(params) == "asc" ? "fr-icon-arrow-up-s-fill" : "fr-icon-arrow-down-s-fill"
+  end
+
+  private
+
+  # Private : Define sort direction.
+  def next_sort_direction(params)
+    current_modified_at_direction(params) == "desc" ? "asc" : "desc"
+  end
+
+  # Private : Define current sort direction.
+  def current_modified_at_direction(params)
+    current_sort = current_q_params(params)["s"].to_s
+    modified_at_sort = current_sort
+                         .split(",")
+                         .map(&:strip)
+                         .find { |sort_value| sort_value.start_with?("modified_at ") }
+
+    modified_at_sort&.split&.last
+  end
+
+  # Private : Get current query params.
+  def current_q_params(params)
+    raw_q =
+      if params[:q].respond_to?(:to_unsafe_h)
+        params[:q].to_unsafe_h
+      else
+        params[:q].to_h
+      end
+
+    raw_q
   end
 end
