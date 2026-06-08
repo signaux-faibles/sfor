fdbebcc04d187d8c70fdfd2e20bbcdf8b36c34fa
Charles Marcoin <charles.marcoin@gmail.com>
2026-06-08 16:54:22 +0200

    Accessibility fixes - lot 1 - quick wins

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,5 +2,10 @@ class Contact < ApplicationRecord
   belongs_to :establishment, foreign_key: :establishment_siret, primary_key: :siret
   include Discard::Model
 
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, allow_blank: true
+  validates :email,
+            format: {
+              with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+              allow_blank: true,
+              message: "Saisir une adresse email valide, par exemple jean.dupont@email.fr" # rubocop:disable Rails/I18nLocaleTexts
+            }
 end
