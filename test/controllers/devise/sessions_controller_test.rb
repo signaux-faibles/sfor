e8eb1f9abc6f4f51d7dd905b1695c0893cce227b
Charles Marcoin <charles.marcoin@gmail.com>
2026-06-11 15:28:58 +0200

    A11Y- add initial focus above flash messages on connexion page

--- test/controllers/devise/sessions_controller_test.rb
+++ test/controllers/devise/sessions_controller_test.rb
@@ -3,19 +3,30 @@
 require "test_helper"
 
 class Devise::SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "new login page exposes initial focus anchor and no email autofocus" do
+  test "new login page exposes initial focus anchor above flash and no email autofocus" do
     get new_user_session_path
 
     assert_response :success
     assert_select "#page-start[data-controller='initial-focus'][tabindex='-1']"
+    assert_select "#page-start + #flash"
     assert_select "input[type=email][autofocus]", false
     assert_select "div[role=alert]", false
   end
 
-  test "failed login renders flash message with alert role" do
+  test "failed login renders flash message with alert role below focus anchor" do
     post user_session_path, params: { user: { email: "wrong@example.com", password: "WrongPass1!" } }
 
     assert_response :unprocessable_content
-    assert_select "div[role=alert]"
+    assert_select "#page-start + #flash div[role=alert]"
+  end
+
+  test "unauthenticated access redirects to login with flash below focus anchor" do
+    get home_path
+
+    assert_redirected_to new_user_session_path
+    follow_redirect!
+
+    assert_response :success
+    assert_select "#page-start + #flash div[role=alert]", text: /Vous devez vous connecter/
   end
 end
